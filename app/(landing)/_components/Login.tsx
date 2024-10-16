79291a32b97922bb8a35f77b89c32b065a3ead30
Denes Beck <denes.beck1@outlook.com>
2024-10-16 20:33:21 +0200

    style: dark mode added to inputs

--- app/(landing)/_components/Login.tsx
+++ app/(landing)/_components/Login.tsx
@@ -1,12 +1,30 @@
 'use client'
-import { Button, InputField } from '@/_components'
+import { Button } from '@/_components'
 import VpnKeyIcon from '@mui/icons-material/VpnKey'
+import EmailIcon from '@mui/icons-material/Email'
+import KeyIcon from '@mui/icons-material/Key'
 import { useState } from 'react'
 import { signInAction } from '@/_actions/auth'
+import {
+  createTheme,
+  ThemeProvider,
+  InputAdornment,
+  TextField,
+} from '@mui/material'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const handleSignIn = async () => {
+    signInAction({ username: email, password })
+  }
 
   return (
     <div className="flex relative z-10 flex-col gap-8 justify-center items-center p-8 from-cyan-300 via-indigo-500 to-blue-400 shadow-md xl:fixed xl:bg-gradient-to-tr xl:rounded-md xl:bottom-[20vh] xl:left-[7vw] xl:w-[28.5rem]">
@@ -14,21 +32,49 @@ const Login = () => {
       <h1 className="z-10 text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-tr from-cyan-300 via-indigo-500 to-blue-400">
         Welcome to NexGen HR
       </h1>
-      <InputField
-        label="Email"
-        value={email}
-        handler={(value: string) => setEmail(value)}
-      />
-      <InputField
-        label="Password"
-        value={password}
-        handler={(value: string) => setPassword(value)}
-        isPassword={true}
-      />
+      <ThemeProvider theme={darkTheme}>
+        <TextField
+          label="Email"
+          variant="outlined"
+          className="w-full"
+          value={email}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(event.target.value)
+          }
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <EmailIcon />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          variant="outlined"
+          className="w-full"
+          value={password}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(() => event.target.value)
+          }
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <KeyIcon />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+      </ThemeProvider>
       <div className="z-10 w-[12rem]">
         <Button
           icon={<VpnKeyIcon className="mr-2" />}
-          action={() => signInAction({ username: email, password })}
+          action={handleSignIn}
           variant="primary-solid"
           label="Login"
           wide={true}
