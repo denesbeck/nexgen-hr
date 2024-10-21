be06137a34391c948c800c12367446eba74fe163
Denes Beck <denes.beck1@outlook.com>
2024-10-21 15:10:09 +0200

    feat: close, purge alerts

--- app/_components/AlertBox.tsx
+++ app/_components/AlertBox.tsx
@@ -1,11 +1,12 @@
 'use client'
 import { Alert } from '@mui/material'
 import { useEffect, useState } from 'react'
-import { CloseButton } from '@/_components'
+
+type Severity = 'error' | 'warning' | 'info' | 'success'
 
 export interface IAlert {
   id: string
-  severity: 'error' | 'warning' | 'info' | 'success'
+  severity: Severity
   message: string
 }
 
@@ -16,13 +17,31 @@ const AlertBox = () => {
     function handleAlert(event: CustomEvent) {
       const alertEventDetail = event.detail as IAlert
       if (alerts.find((alert) => alert.id === alertEventDetail.id)) return
-      setAlerts((prev) => [...prev, alertEventDetail])
+      setAlerts((prev) => {
+        const arr = alerts.length >= 10 ? prev.slice(1) : prev
+        return [...arr, alertEventDetail]
+      })
+    }
+    function handleCloseAlert(event: CustomEvent) {
+      const alertEventDetail = event.detail as IAlert
+      setAlerts((prev) =>
+        prev.filter((alert) => alert.id !== alertEventDetail.id)
+      )
+    }
+    function handlePurgeAlerts() {
+      setAlerts([])
     }
     // @ts-expect-error: https://github.com/microsoft/TypeScript/issues/28357
     window.addEventListener('alert-event', handleAlert)
+    // @ts-expect-error: https://github.com/microsoft/TypeScript/issues/28357
+    window.addEventListener('close-alert', handleCloseAlert)
+    window.addEventListener('purge-alerts', handlePurgeAlerts)
     return () => {
       // @ts-expect-error: https://github.com/microsoft/TypeScript/issues/28357
       window.removeEventListener('alert-event', handleAlert)
+      // @ts-expect-error: https://github.com/microsoft/TypeScript/issues/28357
+      window.removeEventListener('close-alert', handleCloseAlert)
+      window.removeEventListener('purge-alerts', handlePurgeAlerts)
     }
   }, [alerts])
 
--- app/_hooks/useAlert.ts
+++ app/_hooks/useAlert.ts
@@ -1,18 +1,39 @@
 import { IAlert } from '@/_components/AlertBox'
+
+type AlertEventType = 'alert-event' | 'close-alert' | 'purge-alerts'
+
 const useAlert = () => {
   const alert = ({ id, severity, message }: IAlert) => {
     if (window)
       window.dispatchEvent(
-        new CustomEvent('alert-event', {
+        new CustomEvent('alert-event' as AlertEventType, {
           detail: {
             id,
             severity,
             message,
-          },
+          } as IAlert,
         })
       )
   }
-  return { alert }
+  const closeAlert = (id: string) => {
+    if (window)
+      window.dispatchEvent(
+        new CustomEvent('close-alert' as AlertEventType, {
+          detail: {
+            id,
+          } as IAlert,
+        })
+      )
+  }
+  const purgeAlerts = () => {
+    if (window)
+      window.dispatchEvent(
+        new CustomEvent('purge-alerts' as AlertEventType, {
+          detail: {} as IAlert,
+        })
+      )
+  }
+  return { alert, closeAlert, purgeAlerts }
 }
 
 export default useAlert
