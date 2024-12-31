a8fe34b76ec31da4471956786cbbc010cde475dd
Denes Beck <denes.beck1@outlook.com>
2024-12-31 12:26:49 +0100

    refactor: import adj

--- app/(landing)/_components/index.ts
+++ app/(landing)/_components/index.ts
@@ -2,6 +2,6 @@ import Login from './Login'
 import MFAConfirm from './MFAConfirm'
 import MFASetup from './MFASetup'
 import Promo from './Promo'
-import ResetPassword from './reset-password'
+import { ResetPassword } from './reset-password'
 
 export { Login, MFAConfirm, MFASetup, Promo, ResetPassword }
